96ae25c697163b39aec641f1c88f09f832c6c1a2
Alina.Parfenteva <@alinnga>
2022-06-07 16:32:14 +0300

    the report type on the start page is highlighted if select it

--- src/main/resources/static/js/change-report-name.js
+++ src/main/resources/static/js/change-report-name.js
@@ -3,11 +3,15 @@ window.onload = function () {
     const log = document.getElementById('log');
     date.addEventListener('change', updateValue);
 
+    var pathArray = window.location.pathname.split('/');
+    var reportTypeId = pathArray[3];
+    var reportTypeA = document.getElementById("reportType"+reportTypeId);
+    reportTypeA.classList.add("active");
+
     function updateValue(e) {
         var dateValue = e.target.value;
-        var pathArray = window.location.pathname.split('/');
-        var reportTYpeId = pathArray[3];
-        fetch(contextPath + "startPage/reportName/"+dateValue+"/"+reportTYpeId, {method: 'GET'})
+
+        fetch(contextPath + "startPage/reportName/"+dateValue+"/"+reportTypeId, {method: 'GET'})
             .then(response => response.json())
             .then(data => {
                 console.log(data);
@@ -17,14 +21,11 @@ window.onload = function () {
                 for (let i = 0; i < listLength; i++) {
 	                let a = document.createElement('a');
 	                a.setAttribute("href", "#");
+	                a.setAttribute("class", "list-group-item");
 	                a.setAttribute("onclick", "getTagData("+data[i].id+")");
                     a.innerHTML = data[i].name;
 	                reportNames.appendChild(a);
                 }
             })
     }
-}
-
-function selectReportType(element){
-    element.classList.add('active');
 }
