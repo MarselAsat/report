d46b350a62cb02ec16ed3bd1baaf497bdd6e5719
Alina.Parfenteva <@alinnga>
2022-08-08 16:27:46 +0300

    add service for OPC connection

---
+++ src/main/java/com/nppgks/reports/opc/OpcRequests.java
@@ -0,0 +1,38 @@
+package com.nppgks.reports.opc;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class OpcRequests {
+    private final String path="/opc/read";
+
+    private URI uri;
+
+    public OpcRequests(@Value("${opc.host}") String host, @Value("${opc.port}") String port) {
+        uri = UriComponentsBuilder.newInstance()
+                .scheme("http").host(host).port(port).path(path).build().toUri();
+    }
+
+    public Map<String, String> getTagDataFromOpc(List<String> tagNames){
+        RestTemplate restTemplate = new RestTemplate();
+
+        List<String> requestJson = tagNames;
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<List<String>> entity = new HttpEntity<>(requestJson, headers);
+        Map<String, String> map = restTemplate.postForObject(uri, entity, HashMap.class);
+        return map;
+    }
+}
