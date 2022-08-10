164380f7d178c58cd395ac4999f1e1df2d6abe7e
Alina.Parfenteva <@alinnga>
2022-08-10 10:50:22 +0300

    prepare data classes

---
+++ src/main/java/com/nppgks/reports/service/poverka3622/data/FinalData.java
@@ -0,0 +1,40 @@
+package com.nppgks.reports.service.poverka3622.data;
+
+import lombok.Data;
+import lombok.experimental.FieldNameConstants;
+
+@FieldNameConstants
+@Data
+public class FinalData {
+
+    private double[][] f_ij;
+    private double[][] M_e_ij;
+    private double[][] K_ij;
+    private double[][] MF_ij;
+    private double[] Q_j;
+    private double[] f_j;
+    private double[] K_j;
+    private double[] MF_j;
+    private double[] S_j;
+    private double[] S_0j;
+    private double[] t_095;
+    private double[] eps_j;
+    private double[] theta_zj;
+    private double[] theta_sigma_j;
+    private double[] S_theta_j;
+    private double[] S_sigma_j;
+    private double[] t_sigma_j;
+    private double[] delta_j;
+    private double[] Q_min;
+    private double[] Q_max;
+    private double[] eps_PDk;
+    private double[] S_PDk;
+    private double[] theta_PDk;
+    private double[] theta_PDz;
+    private double[] theta_sigma_PDk;
+    private double[] S_theta_PDk;
+    private double[] S_sigma_PDk;
+    private double[] t_sigma_PDk;
+    private double[] delta_PDk;
+
+}
---
+++ src/main/java/com/nppgks/reports/service/poverka3622/data/InitialUnusedData.java
@@ -0,0 +1,20 @@
+package com.nppgks.reports.service.poverka3622.data;
+
+import lombok.Data;
+import lombok.experimental.FieldNameConstants;
+
+@FieldNameConstants
+@Data
+public class InitialUnusedData {
+    private double delta_t_dop;
+    private double delta_P_dop;
+    private double t_min;
+    private double t_max;
+    private double P_min;
+    private double P_max;
+    private double[][] t_ij;
+    private double[][] P_ij;
+    private double[] Q_p;
+    private double[] f_p;
+    private double[] K_y;
+}
