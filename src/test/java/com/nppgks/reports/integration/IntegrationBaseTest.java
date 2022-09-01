f65f5290559775f0903e554550062fd8c0924781
Alina.Parfenteva <@alinnga>
2022-09-01 10:50:44 +0300

    add testcontainers and prepare sql data for tests

---
+++ src/test/java/com/nppgks/reports/integration/IntegrationBaseTest.java
@@ -0,0 +1,22 @@
+package com.nppgks.reports.integration;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@Sql({"classpath:sql/data.sql"})
+public abstract class IntegrationBaseTest {
+    private static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:14.1");
+
+    @BeforeAll
+    static void runContainer() {
+        container.start();
+    }
+
+    @DynamicPropertySource
+    static void postgresProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+    }
+}
--- src/test/resources/sql/data.sql
+++ src/test/resources/sql/data.sql
@@ -1,17 +1,67 @@
+INSERT INTO "user" (username, password, role)
+VALUES ('admin', '$2a$12$uNrGCD3abAGLrDySH2TTB.17nbxrHZrWZ6ZeuW52sOWQKUniq9hlG', 'ROLE_ADMIN'),
+       ('user', '$2a$12$C9C9MlJl/AOOp4UVYkujn.Lxld46KI4SOGXWG34tb0jBE52nKXMGa', 'ROLE_USER');
+
+INSERT INTO report_type (id, name, description, active)
+VALUES (1, 'Часовой', 'Отчеты фомируемые каждый час', TRUE),
+       (2, 'Суточный', 'Отчеты фомируемые за сутки', TRUE),
+       (3, 'Сменный', 'Отчеты фомируемые за смену', TRUE),
+       (4, 'Месячный', 'Отчеты фомируемые за месяц', TRUE),
+       (5, 'Годовой', 'Отчеты формируемые за год', TRUE),
+       (6, 'Ручной', 'Для поверок', TRUE);
+
+INSERT INTO manual_tag_name (id, permanent_name, name, description, initial, type)
+VALUES (1, 'Q_ij', 'tag_'||'Q_ij', '', true, '3622'),
+       (2, 'N_e_ij', 'tag_'||'N_e_ij', '', true, '3622'),
+       (3, 'N_p_ij', 'tag_'||'N_p_ij', '', true, '3622'),
+       (4, 'T_ij', 'tag_'||'T_ij', '', true, '3622'),
+       (5, 'M_ij', 'tag_'||'M_ij', '', true, '3622'),
+       (6, 'f_p_max', 'tag_'||'f_p_max', '', true, '3622'),
+       (7, 'Q_p_max', 'tag_'||'Q_p_max', '', true, '3622'),
+       (8, 'MF_p', 'tag_'||'MF_p', '', true, '3622'),
+       (9, 'K_e_ij', 'tag_'||'K_e_ij', '', true, '3622'),
+       (10, 'ZS', 'tag_'||'ZS', '', true, '3622'),
+       (11, 'theta_e', 'tag_'||'theta_e', '', true, '3622'),
+       (12, 'theta_t', 'tag_'||'theta_t', '', true, '3622'),
+       (13, 'theta_p', 'tag_'||'theta_p', '', true, '3622'),
+       (14, 'theta_N', 'tag_'||'theta_N', '', true, '3622'),
+       (15, 'theta_PDt', 'tag_'||'theta_PDt', '', true, '3622'),
+       (16, 'theta_PDp', 'tag_'||'theta_PDp', '', true, '3622'),
+       (17, 'theta_Dt', 'tag_'||'theta_Dt', '', true, '3622'),
+       (18, 'theta_Dp', 'tag_'||'theta_Dp', '', true, '3622');
+
+INSERT INTO manual_tag_name (id, permanent_name, name, initial, type)
+VALUES (19, 'CPM_name', 'CPM_name', TRUE, '3622'),
+       (20, 'CPM_number', 'CPM_number', TRUE, '3622'),
+       (21, 'CPM_owner', 'CPM_owner', TRUE, '3622'),
+       (22, 'poverka_method', 'poverka_method', TRUE, '3622'),
+       (23, 'poverka_place', 'poverka_place', TRUE, '3622'),
+       (24, 'PR_name', 'PR_name', TRUE, '3622'),
+       (25, 'PR_number', 'PR_number', TRUE, '3622'),
+       (26, 'check_leakproofness', 'check_leakproofness', TRUE, '3622'),
+       (27, 'check_inspection', 'check_inspection', TRUE, '3622'),
+       (28, 'check_software', 'check_software', TRUE, '3622'),
+       (29, 'check_testing', 'check_testing', TRUE, '3622'),
+       (30, 'inspector_position', 'inspector_position', TRUE, '3622'),
+       (31, 'inspector_full_name', 'inspector_full_name', TRUE, '3622'),
+       (32, 'date', 'date', TRUE, '3622');
+SELECT SETVAL('manual_tag_name_id_seq', (SELECT MAX(id) FROM manual_tag_name));
+
+
 INSERT INTO tag_name (id, name, description, report_type_id)
-VALUES (1, 'hour_mass_il1', 'масса за час ил1', 1),
-       (2, 'hour_vol_il1', 'объем за час ил1', 1),
-       (3, 'hour_mass_il2', 'масса за час ил2', 1),
-       (4, 'hour_vol_il2', 'объем за час ил2’', 1),
-       (5, 'hour_sikn_mass', 'масса за час по сикн', 1),
-       (6, 'hour_sikn_vol', 'объем за час по сикн', 1),
-       (7, 'WinCC_OA.report_redu.save', '', 1),
-       (8, 'WinCC_OA.report_redu.main', '', 1),
-       (9, 'WinCC_OA.CRC.Calc_crc', '', 1),
-       (10, 'ns=2;s=Channel1.Device1.Tag3', '', 1),
-       (11, 'ns=2;s=Channel1.Device1.Tag5', '', 1),
-       (12, 'ns=2;s=Channel1.Device1.Tag4', '', 1),
-       (13, 'ns=2;s=Channel1.Device1.Tag6', '', 1);
+VALUES (1, 'hour_mass_il1', 'масса за час ил1', (SELECT id FROM report_type WHERE name = 'Часовой')),
+       (2, 'hour_vol_il1', 'объем за час ил1', (SELECT id FROM report_type WHERE name = 'Часовой')),
+       (3, 'hour_mass_il2', 'масса за час ил2', (SELECT id FROM report_type WHERE name = 'Часовой')),
+       (4, 'hour_vol_il2', 'объем за час ил2’', (SELECT id FROM report_type WHERE name = 'Часовой')),
+       (5, 'hour_sikn_mass', 'масса за час по сикн', (SELECT id FROM report_type WHERE name = 'Часовой')),
+       (6, 'hour_sikn_vol', 'объем за час по сикн', (SELECT id FROM report_type WHERE name = 'Часовой')),
+       (7, 'WinCC_OA.report_redu.save', '', (SELECT id FROM report_type WHERE name = 'Ручной')),
+       (8, 'WinCC_OA.report_redu.main', '', (SELECT id FROM report_type WHERE name = 'Ручной')),
+       (9, 'WinCC_OA.CRC.Calc_crc', '', (SELECT id FROM report_type WHERE name = 'Ручной')),
+       (10, 'ns=2;s=Channel1.Device1.Tag3', '', (SELECT id FROM report_type WHERE name = 'Ручной')),
+       (11, 'ns=2;s=Channel1.Device1.Tag5', '', (SELECT id FROM report_type WHERE name = 'Ручной')),
+       (12, 'ns=2;s=Channel1.Device1.Tag4', '', (SELECT id FROM report_type WHERE name = 'Ручной')),
+       (13, 'ns=2;s=Channel1.Device1.Tag6', '', (SELECT id FROM report_type WHERE name = 'Ручной'));
 SELECT SETVAL('tag_name_id_seq', (SELECT MAX(id) FROM tag_name));
 
 INSERT INTO report_name(id, report_type_id, name, date_creation)
