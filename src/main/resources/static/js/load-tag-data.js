2a5f4c68a0c5e94674d2998cdcac0cae6b268929
Alina.Parfenteva <@alinnga>
2022-10-25 09:31:18 +0300

    display report pages on the start page. Add print button to print a report

--- src/main/resources/static/js/load-tag-data.js
+++ src/main/resources/static/js/load-tag-data.js
@@ -1,23 +1,46 @@
 function getTagData(reportNameId) {
-    fetch(contextPath + "api/tagData/"+reportNameId, {method: 'GET'})
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            const tagDataDiv = document.getElementById("tagData");
-            tagDataDiv.innerHTML = '';
-            var listLength = data.length;
-            for (let i = 0; i < listLength; i++) {
-                let li = document.createElement('tr');
-                console.log(data);
-                li.innerHTML = '<th>'+data[i].tagName+'</th><th>'+data[i].data+'</th><th>'+data[i].date+'</th><th>'+data[i].reportType+'</th>';
-                tagDataDiv.appendChild(li);
-            }
-        })
+    // fetch(contextPath + "api/tagData/"+reportNameId, {method: 'GET'})
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data);
+    //         const tagDataDiv = document.getElementById("tagData");
+    //         tagDataDiv.innerHTML = '';
+    //         var listLength = data.length;
+    //         for (let i = 0; i < listLength; i++) {
+    //             let li = document.createElement('tr');
+    //             console.log(data);
+    //             li.innerHTML = '<th>'+data[i].tagName+'</th><th>'+data[i].data+'</th><th>'+data[i].date+'</th><th>'+data[i].reportType+'</th>';
+    //             tagDataDiv.appendChild(li);
+    //         }
+    //     })
 
-    path = window.location.search;
+    let path = window.location.search;
     const urlParams = new URLSearchParams(path);
     let reportTypeId = urlParams.get("reportTypeId");
-    const showReportRef = document.getElementById("show-report-ref");
-    showReportRef.setAttribute("href", "/"+reportTypeId+"Report/"+reportNameId);
+    const url = "/"+reportTypeId+"Report/"+reportNameId;
 
+    fetch(url).then(function (response) {
+        // The API call was successful!
+        return response.text();
+    }).then(function (html) {
+        // This is the HTML from our response as a text string
+        document.getElementById("includedContent").innerHTML = html
+    }).catch(function (err) {
+        // There was an error
+        console.warn('Something went wrong.', err);
+    });
+}
+
+function printReport(){
+
+    const printContents = document.getElementById("includedContent").innerHTML;
+    let myWindow = window.open();
+
+    myWindow.document.write(printContents);
+    myWindow.document.write('<scr' + 'ipt type="text/javascript">' + 'window.onload = function() { window.print(); window.close(); };' + '</sc' + 'ript>');
+
+    myWindow.document.close(); // necessary for IE >= 10
+    myWindow.focus(); // necessary for IE >= 10
+
+    return true;
 }
