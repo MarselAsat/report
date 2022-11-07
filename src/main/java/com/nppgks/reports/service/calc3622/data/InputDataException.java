01d7562bf6098940e3c3a15f03b5b775e1869560
Alina.Parfenteva <@alinnga>
2022-11-07 11:38:43 +0300

    add new formula for K_e_arr

---
+++ src/main/java/com/nppgks/reports/service/calc3622/data/InputDataException.java
@@ -0,0 +1,7 @@
+package com.nppgks.reports.service.calc3622.data;
+
+public class InputDataException extends RuntimeException{
+    public InputDataException(String message) {
+        super(message);
+    }
+}
