d5149f7d27fdb4306d41fad209af923b46ef5e47
alinnga <alina.parfenteva@mail.ru>
2022-12-26 11:26:48 +0300

    simplify if statement get rid of magic numbers

--- src/main/java/com/nppgks/reportingsystem/reportgeneration/calculations/mi3622/data/DataConverter.java
+++ src/main/java/com/nppgks/reportingsystem/reportgeneration/calculations/mi3622/data/DataConverter.java
@@ -18,13 +18,18 @@ import static java.lang.Integer.parseInt;
 @Slf4j
 public class DataConverter {
 
+    private static final String ARRAY_REGEX = "\\[.*]";
+
+    private static final String CALCULATE_METHOD_PREFIX = "calculate";
+
+
     public static void setCalcFinalDataFields(FinalData finalData, MI3622Calculation calc){
         Field[] declaredFields = FinalData.class.getDeclaredFields();
         Class<? extends MI3622Calculation> calcClass = calc.getClass();
         for(Field field : declaredFields){
             try{
                 field.setAccessible(true);
-                String methodName = "calculate"+ StringUtils.capitalize(field.getName());
+                String methodName = CALCULATE_METHOD_PREFIX + StringUtils.capitalize(field.getName());
                 var value = calcClass.getMethod(methodName).invoke(calc);
                 field.set(finalData, value);
             }
@@ -63,44 +68,36 @@ public class DataConverter {
 
         Field[] declaredFields = InitialData.class.getDeclaredFields();
 
-        for(Field declaredField : declaredFields){
+        for (Field declaredField : declaredFields) {
             try {
                 declaredField.setAccessible(true);
                 String tagName = tagNamesMap.get(declaredField.getName());
-                if(tagName!=null){
+                if (tagName != null) {
                     String value = dataFromOpc.get(tagNamesMap.get(declaredField.getName()));
 
-                    if(value != null){
-                        if(value.matches("\\[\\[.*\\]\\]")){
+                    if (value != null && !value.isBlank()) {
+                        Class<?> fieldType = declaredField.getType();
+
+                        if (fieldType.equals(double[][].class)) {
                             declaredField.set(initialData, to2DArray(value));
-                        }
-                        else if(value.matches("\\[.*\\]")){
+                        } else if (fieldType.equals(double[].class)) {
                             declaredField.set(initialData, toArray(value));
+                        } else if (fieldType.equals(int.class)) {
+                            declaredField.set(initialData, parseInt(value));
+                        } else if (fieldType.equals(double.class)) {
+                            declaredField.set(initialData, parseDouble(value));
+                        } else if (fieldType.equals(boolean.class)) {
+                            declaredField.set(initialData, parseBoolean(value));
+                        } else if (fieldType.equals(String.class)) {
+                            declaredField.set(initialData, value);
                         }
-                        else if(!value.isBlank()){
-                            Class<?> fieldType = declaredField.getType();
-                            if(fieldType.equals(int.class)){
-                                declaredField.set(initialData, parseInt(value));
-                            }
-                            else if(fieldType.equals(double.class)){
-                                declaredField.set(initialData, parseDouble(value));
-                            }
-                            else if(fieldType.equals(boolean.class)){
-                                declaredField.set(initialData, parseBoolean(value));
-                            }
-                            else {
-                                declaredField.set(initialData, value);
-                            }
-                        }
-                    }
-                    else{
+                    } else {
                         log.warn("В полученных данных от OPC нет имя тега {}", declaredField.getName());
                     }
 
-                }
-                else{
+                } else {
                     log.error("В переданной tagNamesMap не нашлось соответствия с полем {} в InitialData ", declaredField.getName());
-                    log.error("Возможно, в таблице manual_tag_name нет нужной записи с permanent_name = {} и initial = true", declaredField.getName());
+                    log.error("Возможно, в таблице calculations.tag_name нет нужной записи с permanent_name = {} и initial = true", declaredField.getName());
                 }
 
             } catch (IllegalAccessException e) {
@@ -115,7 +112,7 @@ public class DataConverter {
         int measureCount = parseInt(dataFromOpc.get(tagNamesMap.get("measureCount")));
         for(Map.Entry<String, String> entry: dataFromOpc.entrySet()){
             String value = entry.getValue();
-            if(value.matches("\\[.*")){
+            if(value.matches(ARRAY_REGEX)){
                 double[] valueArr = toArray(value);
                 if(valueArr.length == pointsCount*measureCount){
                     double[][] array2D = fromArrayTo2DArray(valueArr, pointsCount, measureCount);
