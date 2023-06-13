67fe1c566d0cf6d3c9da9e3f3dedf664a894ffcd
alinnga <alina.parfenteva@mail.ru>
2023-06-13 15:36:42 +0300

    fix mistake that occurred when renaming tables

--- src/main/java/com/nppgks/reportingsystem/constants/SettingsConstants.java
+++ src/main/java/com/nppgks/reportingsystem/constants/SettingsConstants.java
@@ -9,7 +9,7 @@ final public class SettingsConstants {
     public static final String SHIFT_REPORT_COLUMNS = ReportTypesEnum.shift + REPORT_COLUMNS_POSTFIX;
     public static final String MONTH_REPORT_COLUMNS = ReportTypesEnum.month + REPORT_COLUMNS_POSTFIX;
     public static final String YEAR_REPORT_COLUMNS = ReportTypesEnum.year + REPORT_COLUMNS_POSTFIX;
-    public static final String METERING_STATION_NAME = "metering station address";
+    public static final String METERING_STATION_NAME = "metering station name";
     public static final String START_SHIFT_REPORT = ReportTypesEnum.shift + START_TIME_REPORT_POSTFIX;
     public static final String START_DAILY_REPORT = ReportTypesEnum.daily + START_TIME_REPORT_POSTFIX;
     public static final String START_MONTH_REPORT = ReportTypesEnum.month + START_TIME_REPORT_POSTFIX;
