d6bbf5b163e9da62cd6fbe8dc18e7e9f096afe52
alinnga <alina.parfenteva@mail.ru>
2023-06-16 09:02:06 +0300

    add test methods for opcService/readValue

--- src/test/java/com/nppgks/reportingsystem/integration/controller/OpcServiceControllerTest.java
+++ src/test/java/com/nppgks/reportingsystem/integration/controller/OpcServiceControllerTest.java
@@ -8,15 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.client.ResourceAccessException;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureMockMvc
-//@WebMvcTest(OpcServiceController.class)
 @SpringBootTest
 @WithMockUser(username = "testName", password = "test", authorities = "ROLE_ADMIN")
 public class OpcServiceControllerTest extends IntegrationBaseTest{
@@ -62,5 +65,29 @@ public class OpcServiceControllerTest extends IntegrationBaseTest{
         mockMvc.perform(get("/opcService/reconnect"))
                 .andExpect(status().isServiceUnavailable())
                 .andExpect(content().string("OPC сервис недоступен"));
+    }
+
+    @Test
+    void getValueShouldReturnErrorMessageWhenOpcServiceOff() throws Exception {
+        Mockito.when(opcServiceRequests.getTagValuesFromOpc(any(String.class))).thenThrow(ResourceAccessException.class);
+        mockMvc.perform(post("/opcService/readValue").content("tagAddress"))
+                .andExpect(status().isServiceUnavailable())
+                .andExpect(content().string("OPC сервис недоступен"));
+    }
+
+    @Test
+    void getValueShouldReturnTagValueWhenOpcServerHasThisTag() throws Exception {
+        Mockito.when(opcServiceRequests.getTagValuesFromOpc(any(String.class))).thenReturn(new ResponseEntity<>("value", HttpStatus.OK));
+        mockMvc.perform(post("/opcService/readValue").content("tagAddress"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("value"));
+    }
+
+    @Test
+    void getValueShouldReturnNoDataWhenOpcServerDoesntHaveThisTag() throws Exception {
+        Mockito.when(opcServiceRequests.getTagValuesFromOpc(any(String.class))).thenReturn(new ResponseEntity<>("No data", HttpStatus.OK));
+        mockMvc.perform(post("/opcService/readValue").content("tagAddress"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("No data"));
     }
 }
