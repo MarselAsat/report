edd6ae5f0fa955a291ccc16a493da7b04420dcb6
alinnga <alina.parfenteva@mail.ru>
2023-08-18 16:49:30 +0300

    add tags to tag table for moisture meter kmh report

--- src/main/java/com/nppgks/reportingsystem/constants/ManualReportTypes.java
+++ src/main/java/com/nppgks/reportingsystem/constants/ManualReportTypes.java
@@ -1,5 +1,7 @@
 package com.nppgks.reportingsystem.constants;
 
 public enum ManualReportTypes {
-    MI3622, MI3272, ACCEPTANCE_ACT, KMH_VISCOMETER
+    MI3622, MI3272,
+    ACCEPTANCE_ACT,
+    KMH_VISCOMETER, KMH_MOISTURE_METER
 }
