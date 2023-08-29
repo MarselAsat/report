50440d9ffc9504adc34348894618f75fafa5f373
alinnga <alina.parfenteva@mail.ru>
2023-08-29 11:21:15 +0300

    make months in Russian lowercase

--- src/main/java/com/nppgks/reportingsystem/controller/view/ReportViewController.java
+++ src/main/java/com/nppgks/reportingsystem/controller/view/ReportViewController.java
@@ -110,18 +110,18 @@ public class ReportViewController {
         LocalDate creationDate = report.getCreationDt().toLocalDate();
 
         Map<Integer, String> monthsInRussian = new HashMap<>();
-        monthsInRussian.put(1, "Января");
-        monthsInRussian.put(2, "Февраля");
-        monthsInRussian.put(3, "Марта");
-        monthsInRussian.put(4, "Апреля");
-        monthsInRussian.put(5, "Мая");
-        monthsInRussian.put(6, "Июня");
-        monthsInRussian.put(7, "Июля");
-        monthsInRussian.put(8, "Августа");
-        monthsInRussian.put(9, "Сентября");
-        monthsInRussian.put(10, "Октября");
-        monthsInRussian.put(11, "Ноября");
-        monthsInRussian.put(12, "Декабря");
+        monthsInRussian.put(1, "января");
+        monthsInRussian.put(2, "февраля");
+        monthsInRussian.put(3, "марта");
+        monthsInRussian.put(4, "апреля");
+        monthsInRussian.put(5, "мая");
+        monthsInRussian.put(6, "июня");
+        monthsInRussian.put(7, "июля");
+        monthsInRussian.put(8, "августа");
+        monthsInRussian.put(9, "сентября");
+        monthsInRussian.put(10, "октября");
+        monthsInRussian.put(11, "ноября");
+        monthsInRussian.put(12, "декабря");
 
         modelMap.put("dateDay", creationDate.getDayOfMonth());
         modelMap.put("dateYear", creationDate.getYear());
