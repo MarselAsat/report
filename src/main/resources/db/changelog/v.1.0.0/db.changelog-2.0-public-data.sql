d88ac478e93ea0e74d4f979a0291ca37ab3ec4bb
Alina.Parfenteva <alina.parfenteva@mail.ru>
2023-10-26 09:08:24 +0300

    delete 'user' user from user table

--- src/main/resources/db/changelog/v.1.0.0/db.changelog-2.0-public-data.sql
+++ src/main/resources/db/changelog/v.1.0.0/db.changelog-2.0-public-data.sql
@@ -2,8 +2,7 @@
 
 --changeset alina.parfenteva:1
 INSERT INTO "user" (username, password, role)
-VALUES ('admin', '$2a$12$uNrGCD3abAGLrDySH2TTB.17nbxrHZrWZ6ZeuW52sOWQKUniq9hlG', 'ROLE_ADMIN'),
-       ('user', '$2a$12$C9C9MlJl/AOOp4UVYkujn.Lxld46KI4SOGXWG34tb0jBE52nKXMGa', 'ROLE_USER');
+VALUES ('admin', '$2a$12$uNrGCD3abAGLrDySH2TTB.17nbxrHZrWZ6ZeuW52sOWQKUniq9hlG', 'ROLE_ADMIN');
 
 --changeset alina.parfenteva:2
 INSERT INTO settings ("group", name, value)
