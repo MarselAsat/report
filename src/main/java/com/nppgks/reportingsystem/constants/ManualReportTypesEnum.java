3c769baa496b78d5ee3fe6df3aee46e67e0f8e91
Alina Parfenteva <alina.parfenteva@mail.ru>
2023-12-07 15:42:06 +0300

    add tags of mi3313 report to the manual_reports.tag table

--- src/main/java/com/nppgks/reportingsystem/constants/ManualReportTypesEnum.java
+++ src/main/java/com/nppgks/reportingsystem/constants/ManualReportTypesEnum.java
@@ -1,7 +1,7 @@
 package com.nppgks.reportingsystem.constants;
 
 public enum ManualReportTypesEnum {
-    mi3622, mi3272,
+    mi3622, mi3272, mi3313OneEsrm,
     acceptanceAct,
     oilQualityPassport,
     kmhViscometer, kmhMoistureMeter, kmhMassmByMassm, kmhMassmByPu, kmhDensityMeter
